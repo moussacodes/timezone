e2b4bcd67a35dcfde1a3591bfdd19c6e8fc5e73a
duckduckcodes <0x07d2@proton.me>
2023-07-18 10:42:36 +0200

    initial commit

---
+++ components/Container.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  { useEffect, useRef } from 'react';
+
+
+function Container() {
+  return (
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Container
